d42eafa5e8913ed0a8c1b80feb447da1857a304f
Tudor Popescu <tudor.m.p@outlook.com>
2024-07-04 18:07:13 +0300

    Made tools images work; Added tooltips

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     output: 'export',
     reactStrictMode: true,
     images: {
         unoptimized: true, // Disable Next.js built-in image optimization for static export
+        loader: 'custom',
     },
+    basePath: isProd ? '/twdor-portfolio' : '',
+    assetPrefix: isProd ? '/twdor-portfolio/' : '',
 };
 
 export default nextConfig;
