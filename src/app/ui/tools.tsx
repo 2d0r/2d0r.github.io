8fd5bbfa3813435632691e6a9b36f4c8c398fe31
Tudor Popescu <tudor.m.p@outlook.com>
2024-07-04 18:35:41 +0300

    Fixed tooltips

--- src/app/ui/tools.tsx
+++ src/app/ui/tools.tsx
@@ -4,13 +4,10 @@ import '@/app/globals.css';
 
 export default function Tools ({ tools }: { tools: string[] }) {
     const toolsDisplay = tools.map(tool => {
-        // const [ showTooltip, setShowTooltip ] = useState<boolean>(false);
 
         return (<div className='tooltip-container relative' key={tool}>
             <Image src={`/logo/${tool}-w.png`} alt={tool} 
-                height={48} width={48} loader={imageLoader} 
-                // onMouseOver={() => setShowTooltip(true)}
-                // onMouseOut={() => setShowTooltip(false)} 
+                height={48} width={48} loader={imageLoader}
             />
             <div className='tooltip absolute top-[50px] left-auto right-auto bg-white/30 backdrop-blur-xl rounded-lg text-white px-2 py-1'>{tool}</div>
         </div>);
